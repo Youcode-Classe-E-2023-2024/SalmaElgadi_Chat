f367eb133690e16c13fe97a5468bd527399b3068
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-23 21:17:46 +0100

    Update home_view.php

--- views/home_view.php
+++ views/home_view.php
@@ -1,23 +1,27 @@
-<table>
-    <thead>
-    <tr>
-        <th>Email</th>
-        <th>Username</th>
-    </tr>
-    </thead>
-    <tbody>
-
-    <?php foreach ($users as $user) : ?>
-        <tr>
-            <td><?= $user['users_email'] ?></td>
-            <td><?= $user['users_username'] ?></td>
-        </tr>
-    <?php endforeach; ?>
-
-    </tbody>
-</table>
-
-<form action="index.php?page=home" method="POST">
-    <button type="submit" name="login">Login</button>
-    <button type="submit" name="logout">Logout</button>
-</form>
+<div class="bg-gray-50 min-h-screen flex items-center justify-center px-16">
+  <div class="relative w-full max-w-lg">
+    <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob "></div>
+    <div class="absolute top-0 -right-4 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+    <div class="absolute -bottom-32 left-20 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+    <div class="m-8 relative space-y-4">
+      <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
+        <div class="flex-1 flex justify-between items-center">
+          <div class="h-4 w-48 bg-gray-300 rounded"><h1>Welcom on our chat room</h1></div>
+          <div class="w-24 h-6 rounded-lg bg-purple-300"></div>
+        </div>
+      </div>
+      <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
+        <div class="flex-1 flex justify-between items-center">
+          <div class="h-4 w-48 bg-gray-300 rounded"><h1>u don't have an account</h1></div>
+          <div class="w-24 h-6 rounded-lg bg-purple-300"><a href="index.php?page=register">Register</a></div>
+        </div>
+      </div>
+      <div class="p-5 bg-white rounded-lg flex items-center justify-between space-x-8">
+        <div class="flex-1 flex justify-between items-center">
+          <div class="h-4 w-48 bg-gray-300 rounded"><h1>Already have an account</h1></div>
+          <div class="w-24 h-6 rounded-lg bg-purple-300"><a href="index.php?page=login">Log in</a></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
