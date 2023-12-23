41c97515ab94207e0ae6094ab71c2db45436d6fe
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-23 21:18:38 +0100

    Update home_controller.php

--- controllers/home_controller.php
+++ controllers/home_controller.php
@@ -1,11 +1,10 @@
 <?php
 
-echo '<h3>- Home Controller -</h3>';
+// echo '<h3>- Home Controller -</h3>';
 
-if (isset($_POST['login'])) {
-    echo 'Is login';
-}
+// if (isset($_POST['login'])) {
+//     echo 'Is login';
+//     header('Location:index.php?page=login');}
 
-if (isset($_POST['logout'])) {
-    echo 'Is logout';
-}
+// if (isset($_POST['logout'])) {
+//     header('Location:index.php?page=register');}
