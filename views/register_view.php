1299090a1f92d18703585e7cdc9ec6c4b12ebb59
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-23 21:17:43 +0100

    Create register_view.php

---
+++ views/register_view.php
@@ -0,0 +1,28 @@
+<div class="login-box">
+  <h2>Register</h2>
+  <form>
+    <div class="user-box">
+      <input type="text" name="username" required="">
+      <label>User name</label>
+    </div>
+    <div class="user-box">
+      <input type="email" name="email" required="">
+      <label>Email</label>
+    </div>
+    <div class="user-box">
+      <input type="password" name="password" required="">
+      <label>Password</label>
+    </div>
+    <div class="user-box">
+      <input type="text" name="photo" required="">
+      <label>Photo</label>
+    </div>
+    <a href="#">
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      Register
+    </a>
+  </form>
+</div>
