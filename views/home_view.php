d7b1b17da0cfcad4ec128cc462739629a718f3a1
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-24 14:39:45 +0100

    Update home_view.php

--- views/home_view.php
+++ views/home_view.php
@@ -1,3 +1,4 @@
+
 <div class="bg-gray-50 min-h-screen flex items-center justify-center px-16">
   <div class="relative w-full max-w-lg">
     <div class="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob "></div>
