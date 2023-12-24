f042ed943e7f71a5bef15d0cdb63013d6d38d65a
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-25 00:31:59 +0100

    Update add_friend_controller.php

--- controllers/add_friend_controller.php
+++ controllers/add_friend_controller.php
@@ -2,4 +2,17 @@
 
 include_once 'models/User.php';
 
-$users = new  User();
+$User = new  User();
+
+$users = $User->getAllUsers();
+
+if(isset($_POST['addfriend']))
+{
+    $id_me = $_POST['myid'];
+    $id_user = $_POST['friendid'];
+    
+}
+else
+{
+
+}
