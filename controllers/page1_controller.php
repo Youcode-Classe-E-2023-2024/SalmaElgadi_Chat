35c62ffaa3b52f47a78c46ec9efa99bec9f849b6
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-24 16:42:03 +0100

    Update page1_controller.php

--- controllers/page1_controller.php
+++ controllers/page1_controller.php
@@ -1,14 +1,11 @@
 <?php
-echo"hhcontro";
-if (isset($_POST['logout'])) {
-    echo 'Is logout';
+
+if (isset($_POST['logout'])) 
+{
     session_start();
-
-$_SESSION = array();
-
-session_destroy();
-
-header("Location:index.php?page=login");
-exit();
+    $_SESSION = array();
+    session_destroy();
+    header("Location:index.php?page=login");
+    exit();
    
 }
