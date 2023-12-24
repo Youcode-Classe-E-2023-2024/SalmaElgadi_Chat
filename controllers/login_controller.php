8bf62854a5a6bc7df2b5786445d9db9523178361
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-24 16:42:07 +0100

    Update login_controller.php

--- controllers/login_controller.php
+++ controllers/login_controller.php
@@ -4,25 +4,29 @@ include_once '_classes/User.php';
 
 $users = new  User();
 
-if ($_SERVER["REQUEST_METHOD"] === 'POST') {
+if ($_SERVER["REQUEST_METHOD"] === 'POST') 
+{
     $email = $_POST['email'];
     $password = $_POST['password'];
 
     $userInfo = $users->login($email, $password);
     
 
-    if ($userInfo) {
+    if ($userInfo) 
+    {
         session_start();
 
         $_SESSION['id_user'] = $userInfo['id_user'];
         $_SESSION['username'] = $userInfo['username'];
-    
-        echo "login";
+        $_SESSION['email'] = $userInfo['email'];
+
         header("location:index.php?page=page1");
         exit();
-    } else {
+    } 
+    else 
+    {
         echo "not log";
-        // header("Location: ../../view/User/connect.php?STATUS=probleme_de_connexion");
+        // header("location:index.php?page=login?STATUS=probleme_de_connexion");
         exit();
     }
 }
