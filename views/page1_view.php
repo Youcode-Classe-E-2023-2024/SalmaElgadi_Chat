35d4cb01c43696ce1468ab601f03e86989f31bf9
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-24 14:39:39 +0100

    Update page1_view.php

--- views/page1_view.php
+++ views/page1_view.php
@@ -1,3 +1,10 @@
-<form action="index.php?page=page1" method="post">
+<?php
+if(!isset($_SESSION['id_user'])){
+    header("location:index.php?page=login");
+}
+?>
+
+
+<form action="index.php?page=page1" method="POST">
     <button type="submit" name="logout">Logout</button>
 </form>
