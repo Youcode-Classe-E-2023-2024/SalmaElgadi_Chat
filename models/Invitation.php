4a601ec968635d907cf9c99f63111ad29cf05dc1
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-25 22:26:21 +0100

    Update Invitation.php

--- models/Invitation.php
+++ models/Invitation.php
@@ -11,7 +11,7 @@ class Invitation
     public function send_invitation($id_me, $id_user)
     {
         global $db;
-        $stmt = $db->prepare("INSERT INTO invitation (id_user1, id_user2) VALUES ('$id_me','$id_user')");
+        $stmt = $db->prepare("INSERT INTO invitation (id_sender, id_receiver) VALUES ('$id_me','$id_user')");
         $result = $stmt->execute();
         return $result;
     }
@@ -19,13 +19,29 @@ class Invitation
     public function getInvitation()
     {
         global $db;
-        $result = $db->query("SELECT username, email FROM users WHERE id_user IN (SELECT id_user1 FROM invitation WHERE statut='0')");
+        $result = $db->query("SELECT * FROM users WHERE id_user IN (SELECT id_sender FROM invitation WHERE statut='0')");
         return $result->fetch_all(MYSQLI_ASSOC);
     }
-
-    public function delete_invitation($id_me, $id_user)
+    public function acceptInvitation($id_me, $id_user)
     {
-        
+        global $db;
+        $stmt = $db->prepare("UPDATE invitation SET statut='1' WHERE id_receiver='$id_me' AND id_sender='$id_user'");
+        $result = $stmt->execute();
+        return $result;
     }
 
+    public function deleteInvitation($id_me, $id_user)
+    {
+       global $db;
+       $stmt = $db->prepare("DELETE FROM invitation  WHERE id_receiver='$id_me' AND id_sender='$id_user'");
+       $result = $stmt->execute();
+       return $result;
+    }
+
+    public function getFriends()
+    {
+        global $db;
+        $result = $db->query("SELECT * FROM users WHERE id_user IN (SELECT id_sender FROM invitation WHERE statut='1')");
+        return $result->fetch_all(MYSQLI_ASSOC);
+    }
 }
