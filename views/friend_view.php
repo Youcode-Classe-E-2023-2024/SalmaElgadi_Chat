ab26db8c0f6d6c2efa13bf0d20b4378662b3014f
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-25 22:26:15 +0100

    Update friend_view.php

--- views/friend_view.php
+++ views/friend_view.php
@@ -59,7 +59,7 @@ if(!isset($_SESSION['id_user'])){
         <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
         
         <form action="index.php?page=page1" method="POST">
-        <button type="submit" name="logout">Log out</button>
+        <button type="submit" name="logout"><img src="<?= PATH ?>assets/image/logout.png" alt=""></button>
         </form>
         </div>
 
@@ -98,23 +98,39 @@ if(!isset($_SESSION['id_user'])){
     <div class="px-3 py-4 flex justify-center">
         <table class="w-2/3	 text-md bg-white shadow-md rounded mb-4">
             <tbody>
-                <tr class="border-b">
-                    <th class="text-left p-3 px-5">User Name</th>
-                    <th class="text-left p-3 px-5">Email</th>
-                    <th></th>
-                </tr>
-                <?php
-                    if (!empty($invitations)) {
-                    foreach ($invitations as $invitation) {
-                ?>
-                <tr class="border-b hover:bg-orange-100 bg-gray-100">
-                    <td class="p-3 px-5"><?php echo $invitation['username'] ;?></td>
-                    <td class="p-3 px-5"><?php echo $invitation['email'] ;?></td>
-                    <td class="p-3 px-5 flex justify-end"><button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Accepter</button><button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Refuser</button></td>
-                </tr>
-                <?php
-                    }}
-                ?>
+
+            <tr class="border-b">
+                <th class="text-left p-3 px-5">User Name</th>
+                <th class="text-left p-3 px-5">Email</th>
+                <th></th>
+            </tr>
+            <?php
+                if (!empty($invitations)) {
+                foreach ($invitations as $invitation) {
+            ?>
+            <tr class="border-b hover:bg-orange-100 bg-gray-100">
+                <td class="p-3 px-5"><?php echo $invitation['username'] ;?></td>
+                <td class="p-3 px-5"><?php echo $invitation['email'] ;?></td>
+                <td class="p-3 px-5 flex justify-end">
+
+                <form action="<?= PATH ?>index.php?page=friend" method="post">
+                    <input type="hidden" name="myid" value="<?php echo $_SESSION['id_user']; ?>">
+                    <input type="hidden" name="friendid" value="<?php echo $invitation['id_user'] ; ?>">
+                    <button type="submit" name="accept" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Accepter</button>
+                </form>
+
+                <form action="<?= PATH ?>index.php?page=friend" method="post">
+                    <input type="hidden" name="myid" value="<?php echo $_SESSION['id_user']; ?>">
+                    <input type="hidden" name="friendid" value="<?php echo $invitation['id_user'] ; ?>">
+                    <button type="submit" name="decline" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Refuser</button></td>
+                </form> 
+            </tr>
+            <?php
+                }}else{?>
+                <td class="text-red-700 p-3 px-5"><?php echo 'Aucune invitation trouvée';?></td>
+                <?php }
+            ?>
+
             </tbody>
         </table>
     </div>
@@ -135,11 +151,20 @@ if(!isset($_SESSION['id_user'])){
                     <th class="text-left p-3 px-5">Email</th>
                     <th></th>
                 </tr>
+                <?php
+                if (!empty($amis)) {
+                foreach ($amis as $ami) {
+                ?>
                 <tr class="border-b hover:bg-orange-100 bg-gray-100">
-                    <td class="p-3 px-5"><input type="text" value="user.name" class="bg-transparent"></td>
-                    <td class="p-3 px-5"><input type="text" value="user.email" class="bg-transparent"></td>
+                    <td class="p-3 px-5"><?php echo $ami['username'] ;?></td>
+                    <td class="p-3 px-5"><?php echo $ami['email'] ;?></td>
                     <td class="p-3 px-5 flex justify-end"><button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Supprimer</button></td>
                 </tr>
+                <?php
+                }}else{?>
+                <td class="text-red-700 p-3 px-5"><?php echo 'Aucun ami trouvé ';?></td>
+                <?php }
+            ?>
             </tbody>
         </table>
     </div>
