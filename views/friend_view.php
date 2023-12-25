3cd20c27b9569dbb68efc86eeb5622c886212200
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-25 15:13:34 +0100

    Update friend_view.php

--- views/friend_view.php
+++ views/friend_view.php
@@ -89,6 +89,62 @@ if(!isset($_SESSION['id_user'])){
     <main class="max-w-full ml-34 h-full flex flex-col gap-5 py-24 relative overflow-y-hidden">
     <h1 class="text-4xl text-yellow-400 py-4 text-center font-bold">My Friends</h1>
 
+    <!-- invitation -->
+    <div class="pl-80 flex">
+        <h1 class="text-3xl">
+            Invitations
+        </h1>
+    </div>
+    <div class="px-3 py-4 flex justify-center">
+        <table class="w-2/3	 text-md bg-white shadow-md rounded mb-4">
+            <tbody>
+                <tr class="border-b">
+                    <th class="text-left p-3 px-5">User Name</th>
+                    <th class="text-left p-3 px-5">Email</th>
+                    <th></th>
+                </tr>
+                <?php
+                    if (!empty($invitations)) {
+                    foreach ($invitations as $invitation) {
+                ?>
+                <tr class="border-b hover:bg-orange-100 bg-gray-100">
+                    <td class="p-3 px-5"><?php echo $invitation['username'] ;?></td>
+                    <td class="p-3 px-5"><?php echo $invitation['email'] ;?></td>
+                    <td class="p-3 px-5 flex justify-end"><button type="button" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Accepter</button><button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Refuser</button></td>
+                </tr>
+                <?php
+                    }}
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <!--  -->
+
+
+    <!-- Mes amis -->
+    <div class="pl-80 flex">
+        <h1 class="text-3xl">
+            Mes amis
+        </h1>
+    </div>
+    <div class="px-3 py-4 flex justify-center">
+        <table class="w-2/3	 text-md bg-white shadow-md rounded mb-4">
+            <tbody>
+                <tr class="border-b">
+                    <th class="text-left p-3 px-5">User Name</th>
+                    <th class="text-left p-3 px-5">Email</th>
+                    <th></th>
+                </tr>
+                <tr class="border-b hover:bg-orange-100 bg-gray-100">
+                    <td class="p-3 px-5"><input type="text" value="user.name" class="bg-transparent"></td>
+                    <td class="p-3 px-5"><input type="text" value="user.email" class="bg-transparent"></td>
+                    <td class="p-3 px-5 flex justify-end"><button type="button" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Supprimer</button></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    <!--  -->
+
     </main>
     </div>
 
