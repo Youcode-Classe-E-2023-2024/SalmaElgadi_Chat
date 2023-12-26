f7c200ba360dcc71e5a2abd59ccb683135069b3a
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-27 00:06:25 +0100

    Update Room.php

--- models/Room.php
+++ models/Room.php
@@ -4,31 +4,33 @@ class Room
 {
     public function __construct()
     {
-        global $db;
+        
     }
     
     public function addRoom($title,$created_by, $selectedUsers)
     {
         {
-            $this->db->begin_transaction();
+            global $db;
+
+            $db->begin_transaction();
     
-            $stmt1 = $db->prepare("INSERT INTO ticket (title, created_by) VALUES (?, ?)");
+            $stmt1 = $db->prepare("INSERT INTO rooms (title, created_by) VALUES (?, ?)");
             $stmt1->bind_param("si", $title, $created_by);
     
             if (!$stmt1->execute()) {
-                $this->db->rollback();
+                $db->rollback();
                 return false;
             }
     
-            $ticketId = $this->db->insert_id;
+            $ticketId = $db->insert_id;
     
     
             foreach ($selectedUsers as $userId) {
-                $stmt2 = $this->db->prepare("INSERT INTO room (id_r, id_u) VALUES (?, ?)");
+                $stmt2 = $db->prepare("INSERT INTO room (id_r, id_u) VALUES (?, ?)");
                 $stmt2->bind_param("ii", $ticketId, $userId);
     
                 if (!$stmt2->execute()) {
-                    $this->db->rollback();
+                    $db->rollback();
                     return false;
                 }
     
