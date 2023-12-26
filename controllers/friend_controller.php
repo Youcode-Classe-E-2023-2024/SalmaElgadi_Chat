a98c1e18dd34ef4fcf84375af7475cc79504a95a
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-26 16:16:18 +0100

    Update friend_controller.php

--- controllers/friend_controller.php
+++ controllers/friend_controller.php
@@ -14,7 +14,9 @@ if(isset($_POST['accept']))
 {
     $id_me = $_POST['myid'];
     $id_user = $_POST['friendid'];
-    $Invitation->acceptInvitation($id_me, $id_user);
+    if($Invitation->acceptInvitation($id_me, $id_user)){
+        $Invitation->deleteInvitation($id_me, $id_user);
+    }
 
 }
 if(isset($_POST['decline']))
@@ -28,7 +30,12 @@ if(isset($_POST['supprimer']))
 {
     $id_me = $_POST['myid'];
     $id_user = $_POST['friendid'];
-    $Invitation->deleteFriend($id_me, $id_user);
+    if($Invitation->deleteFriend($id_me, $id_user)){
+        echo'hhh';
+    }
+else{
+    echo 'hhhaaaaaaaaaa';
+}
 
 }
 
