eb50fd1ceaa1a76778ec6834f493981dddcbd456
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-26 10:27:45 +0100

    Update add_friend_controller.php

--- controllers/add_friend_controller.php
+++ controllers/add_friend_controller.php
@@ -6,13 +6,21 @@ include_once 'models/Invitation.php';
 $User = new  User();
 $Invitation = new  Invitation();
 
+$myId = $_SESSION['id_user'];
 
-$users = $User->getAllUsers();
+$users = $User->getAllUsers($myId);
 
 if(isset($_POST['addfriend']))
 {
     $id_me = $_POST['myid'];
     $id_user = $_POST['friendid'];
-    $Invitation->send_invitation($id_me, $id_user);
+    // $Invitation->send_invitation($id_me, $id_user);
+    if($Invitation->send_invitation($id_me, $id_user))
+    {
+        echo'wee';
+    }
+    else{
+        echo 'laaa';
+    }
 
 }
