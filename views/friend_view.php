99b12a2e50cf0bc7806c7f48818c42f354c30b3e
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-26 16:15:58 +0100

    Update friend_view.php

--- views/friend_view.php
+++ views/friend_view.php
@@ -38,9 +38,7 @@ if(!isset($_SESSION['id_user'])){
         <!-- Theme -->
         <a href="#">
             <div class="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:text-gray-800 hover:bg-white  hover:duration-300 hover:ease-linear focus:bg-white">
-            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
-            </svg>
+            <img src="<?= PATH ?>assets/image/room.png" alt="">
             </div>  
         </a>
         
@@ -162,7 +160,7 @@ if(!isset($_SESSION['id_user'])){
                         <form action="<?= PATH ?>index.php?page=friend" method="post">
                             <input type="hidden" name="myid" value="<?php echo $_SESSION['id_user']; ?>">
                             <input type="hidden" name="friendid" value="<?php echo $invitation['id_user'] ; ?>">
-                            <button type="submit" name="supprimer" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Supprimer</button></td>
+                            <button type="submit" name="supprimer" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Supprimer</button>
                         </form> 
                     </td>
                 </tr>
