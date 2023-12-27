82b38c5a7ebe921b36ae49b47458154d5827aec9
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-28 00:15:38 +0100

    Update chat_controller.php

--- controllers/chat_controller.php
+++ controllers/chat_controller.php
@@ -1,31 +1,35 @@
 <?php
-
 include_once 'models/Room.php';
 
-
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-   
+    // Récupérer les données JSON envoyées depuis le client
     $inputJSON = file_get_contents('php://input');
-
-    // Decode the JSON data
     $data = json_decode($inputJSON, true);
-    $data = [
-        "message" => $data['message'],
-        "roomId"=>$_SESSION['id_user'],
-    ];
-    // if ($Room->sendMessage($myId, $id, $message)) {
-        echo json_encode(["data" => $data]);
-        
-    // } else {
-        // echo json_encode(["message" => false]);
-    // }
+
+    // Récupérer les données nécessaires
+    $roomId = $data['roomId'];
+    $message = $data['message'];
+    $myId = $_SESSION['id_user'];
+
+    // Ajouter le message à la base de données
+    $Room = new Room();
+    if ($Room->sendMessage($myId, $roomId, $message)) {
+        echo json_encode(["success" => true]);
+    } else {
+        echo json_encode(["success" => false]);
+    }
+
+    // Terminer le script
     die();
 } else {
-    
-$Room = new Room();
+    // Si la requête n'est pas une requête POST, traiter les autres cas
+    $Room = new Room();
+    $id = $_GET['id'];
+    $rooms = $Room->getRoomById($id);
+    $messages = $Room->getMessages($id);
 
-$id = $_GET['id'];
-
-$rooms = $Room->getRoomById($id);
-$messages = $Room->getMessages($id);
+    // Ne renvoyer le JSON que si vous n'êtes pas en train de traiter une requête POST
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        echo json_encode(["success" => true, "messages" => $messages]);
+    }
 }
