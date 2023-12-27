a05877d57266d84af43517c346cb74ab1219b7d3
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-27 02:01:14 +0100

    Update page1_view.php

--- views/page1_view.php
+++ views/page1_view.php
@@ -7,11 +7,23 @@ if(!isset($_SESSION['id_user'])){
 <h1 class="text-4xl text-yellow-400  text-center font-bold">Your rooms</h1>
 <!-- Container -->
 <div class="h-full  mx-24 flex flex-wrap items-start justify-start rounded-tl grid-flow-col auto-cols-max gap-4 ">
+  <?php
+      if (!empty($rooms)) {
+      foreach ($rooms as $room) {
+  ?>
   <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400">
+    <div class="mt-8 flex justify-center">
+      <div>
+          <p class="text-center text-4xl font-extrabold text-[#FE5401]"><?php echo $room['title']; ?></p>
+          <p class="text-center text-2xl font-extrabold text-[#FE5401]"><ins>Membre :</ins></p>
 
+      </div>  
+    </div>
   </div>
+<?php }}else{ ?>
+  <p class="text-center text-2xl font-extrabold text-[#FE5401]">Aucune room trouve</p>
+  <?php }?>
   
-  <div class="w-96 h-60 rounded-lg flex-shrink-0 flex-grow bg-gray-400">
     
   </div>
 </div>  
