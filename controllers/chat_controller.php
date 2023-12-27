2872173b75fc4aba30f95b9cd4ff0d3e85d65e57
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-27 21:49:28 +0100

    Update chat_controller.php

--- controllers/chat_controller.php
+++ controllers/chat_controller.php
@@ -2,8 +2,30 @@
 
 include_once 'models/Room.php';
 
-$Room = new  Room();
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+   
+    $inputJSON = file_get_contents('php://input');
+
+    // Decode the JSON data
+    $data = json_decode($inputJSON, true);
+    $data = [
+        "message" => $data['message'],
+        "roomId"=>$_SESSION['id_user'],
+    ];
+    // if ($Room->sendMessage($myId, $id, $message)) {
+        echo json_encode(["data" => $data]);
+        
+    // } else {
+        // echo json_encode(["message" => false]);
+    // }
+    die();
+} else {
+    
+$Room = new Room();
 
 $id = $_GET['id'];
 
 $rooms = $Room->getRoomById($id);
+$messages = $Room->getMessages($id);
+}
