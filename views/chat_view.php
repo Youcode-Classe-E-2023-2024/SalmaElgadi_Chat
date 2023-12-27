8c37954f85c6362483597c92bf91f91b82168e14
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-27 11:40:45 +0100

    Update chat_view.php

--- views/chat_view.php
+++ views/chat_view.php
@@ -3,154 +3,79 @@ if(!isset($_SESSION['id_user'])){
     header("location:index.php?page=login");
 }
 ?>
+<div class="flex flex-col items-center justify-center w-full h-1/2 text-gray-800 p-5">
 
-<div class="flex justify-center">
 
-<div class=" h-screen">
-<div class="flex border border-grey rounded shadow-lg w-full h-full">
-<!-- Right -->
-<div class="w-full border flex flex-col">
+<!-- Component Start -->
+<div class="flex flex-col flex-grow w-full max-w-xl shadow-xl rounded-lg overflow-hidden">
+    <div class="flex flex-col flex-grow h-0  overflow-auto">
+    <div class="fixed " style="width: 35rem;">
+    <div class="py-2 rounded-md	px-3 flex flex-row justify-between bg-purple-200 items-center">
 
-<!-- Header -->
-<div class="py-2 px-3 bg-grey-lighter flex flex-row justify-between items-center">
+    <form action="index.php?page=chat" method="get">
+    <!-- Room name -->
     <div class="flex items-center">
         <div>
             <img class="w-10 h-10 rounded-full" src="<?= PATH ?>assets/image/22072231_6563382.jpg"/>
         </div>
         <div class="ml-4">
+            <?php
+            foreach ($rooms as $room) {
+            ?>
             <p class="text-grey-darkest">
-                Room name
+            <?php echo $room['title']; ?>
             </p>
-            <p class="text-grey-darker text-xs mt-1">
-                Members
-            </p>
+            <?php 
+            }
+            ?>
         </div>
     </div>
-</div>
-
-<!-- Messages -->
-<div class="flex-1 overflow-auto" style="background-color: #DAD3CC">
-    <div class="py-2 px-3">
-
-        <div class="flex justify-center mb-4">
-            <div class="rounded py-2 px-4" style="background-color: #FCF4CB">
-                <p class="text-xs">
-                    Messages to this chat and calls are now secured with end-to-end encryption. Tap for more info.
-                </p>
-            </div>
-        </div>
-
-        <div class="flex mb-2">
-            <div class="rounded py-2 px-3" style="background-color: #F2F2F2">
-                <p class="text-sm text-teal">
-                    Sylverter Stallone
-                </p>
-                <p class="text-sm mt-1">
-                    Hi everyone! Glad you could join! I am making a new movie.
-                </p>
-                <p class="text-right text-xs text-grey-dark mt-1">
-                    12:45 pm
-                </p>
-            </div>
-        </div>
-
-        <div class="flex mb-2">
-            <div class="rounded py-2 px-3" style="background-color: #F2F2F2">
-                <p class="text-sm text-purple">
-                    Tom Cruise
-                </p>
-                <p class="text-sm mt-1">
-                    Hi all! I have one question for the movie
-                </p>
-                <p class="text-right text-xs text-grey-dark mt-1">
-                    12:45 pm
-                </p>
-            </div>
-        </div>
-
-        <div class="flex mb-2">
-            <div class="rounded py-2 px-3" style="background-color: #F2F2F2">
-                <p class="text-sm text-orange">
-                    Harrison Ford
-                </p>
-                <p class="text-sm mt-1">
-                    Again?
-                </p>
-                <p class="text-right text-xs text-grey-dark mt-1">
-                    12:45 pm
-                </p>
-            </div>
-        </div>
-
-        <div class="flex mb-2">
-            <div class="rounded py-2 px-3" style="background-color: #F2F2F2">
-                <p class="text-sm text-orange">
-                    Russell Crowe
-                </p>
-                <p class="text-sm mt-1">
-                    Is Andrés coming for this one?
-                </p>
-                <p class="text-right text-xs text-grey-dark mt-1">
-                    12:45 pm
-                </p>
-            </div>
-        </div>
-
-        <div class="flex mb-2">
-            <div class="rounded py-2 px-3" style="background-color: #F2F2F2">
-                <p class="text-sm text-teal">
-                    Sylverter Stallone
-                </p>
-                <p class="text-sm mt-1">
-                    He is. Just invited him to join.
-                </p>
-                <p class="text-right text-xs text-grey-dark mt-1">
-                    12:45 pm
-                </p>
-            </div>
-        </div>
-
-        <div class="flex justify-end mb-2">
-            <div class="rounded py-2 px-3" style="background-color: #E2F7CB">
-                <p class="text-sm mt-1">
-                    Hi guys.
-                </p>
-                <p class="text-right text-xs text-grey-dark mt-1">
-                    12:45 pm
-                </p>
-            </div>
-        </div>
-
-        <div class="flex justify-end mb-2">
-            <div class="rounded py-2 px-3" style="background-color: #E2F7CB">
-                <p class="text-sm mt-1">
-                    Count me in
-                </p>
-                <p class="text-right text-xs text-grey-dark mt-1">
-                    12:45 pm
-                </p>
-            </div>
-        </div>
-
-        <div class="flex mb-2">
-            <div class="rounded py-2 px-3" style="background-color: #F2F2F2">
-                <p class="text-sm text-purple">
-                    Tom Cruise
-                </p>
-                <p class="text-sm mt-1">
-                    Get Andrés on this movie ASAP!
-                </p>
-                <p class="text-right text-xs text-grey-dark mt-1">
-                    12:45 pm
-                </p>
-            </div>
-        </div>
-
+    <!--  -->
     </div>
-</div>
+    </div>
 
+    <div class="flex w-full pt-20 space-x-3 max-w-xs">
+        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+        <div>
+            <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+            </div>
+            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+        </div>
+    </div>
+    <div class="flex w-full mt-2 space-x-3 max-w-xs">
+        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+        <div>
+            <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+        </div>
+    </div>
+    <div class="flex w-full mt-2 space-x-3 max-w-xs">
+        <div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+        <div>
+            <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+                <p class="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+            </div>
+            <span class="text-xs text-gray-500 leading-none">2 min ago</span>
+        </div>
+    </div>
+    </div>
 
+    </form>
+    <!-- inputs -->
 
+    <div class="bg-purple-100 flex gap-2 w-full p-4">
+        <input class="flex items-center h-10 w-3/4 rounded py-1 px-3 text-sm" type="text" placeholder="Type your message…">
+        <div class="-mr-1 ">  
+            <button type='submit' name="sendMessage" class="bg-blue-200 text-gray-700 font-medium py-2 px-3 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100">Envoyer</button>
+        </div>
+    </div>  
+
+    <!--  -->
+    </div>
+
+	<!-- Component End  -->
+    
 </div>
-</div>
-</div>  
