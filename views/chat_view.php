84c85b609a053170fdeb546b06fc586809e955c5
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-28 11:38:34 +0100

    Update chat_view.php

--- views/chat_view.php
+++ views/chat_view.php
@@ -8,8 +8,9 @@ if (!isset($_SESSION['id_user'])) {
 <div class="flex flex-col items-center justify-center w-full h-screen text-gray-800 p-5">
     
     <!-- Component Start -->
-    <div class="flex flex-col flex-grow w-full max-w-xl shadow-xl  rounded-lg overflow-hidden">
+    <div class="flex flex-col flex-grow w-full  max-w-xl shadow-xl  rounded-lg overflow-hidden">
     <div class="flex flex-col flex-grow h-0 p-2 overflow-auto">
+        <div class="h-3/4 overflow-auto">
     <div class="fixed " style="width: 35rem;">
         <div class="py-2 rounded-md px-3 flex flex-row justify-between bg-purple-200 items-center">
 
@@ -53,6 +54,7 @@ if (!isset($_SESSION['id_user'])) {
         <?php
         }
         ?>
+        </div>
 
         <!-- inputs -->
         <div class="bg-purple-100  flex content-end	 gap-2 w-full  p-4">
@@ -70,71 +72,3 @@ if (!isset($_SESSION['id_user'])) {
 </div>
 </div>
 
-<script>
-    
-    function sendMessage() {
-    const messageText = document.getElementById('messageInput').value.trim();
-    const roomIdInput = document.getElementById('roomId');
-    const roomId = roomIdInput.value;
-
-    if (messageText !== '') {
-        fetch('index.php?page=chat', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                roomId: roomId,
-                message: messageText,
-            }),
-        })
-        .then(response => response.json())
-        .then(response => {
-            console.log('Server response:', response);
-            if (response.success) {
-                displayData(response.messages);
-            }
-        })
-        .catch(error => console.error('Error', error));
-
-        document.getElementById('messageInput').value = '';
-    }
-}
-
-//     function displayData(messages) {
-//     console.log('Displaying data:', messages);
-
-//     const rows = messages.map((message) => {
-//             return (`
-//             <div class="flex w-full mt-2 space-x-3 max-w-xs">
-// 				<div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
-// 				<div>
-// 					<div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
-//                     <p class="text-sm">${message.text}</p>
-//                     </div>
-// 				</div>
-// 			</div>
-//         `);
-//     })
-//     document.getElementById('chatContainer').innerHTML = rows.join('');
-
-//     function ff(){
-//         var urlParams = new URLSearchParams(window.location.search);
-//         var id = urlParams.get('id');
-//         var url1 = `index.php?page=message&room=${id}`;
-        
-//         fetch(url1, {
-//             method: 'POST',
-//         })
-//         .then((responseData) => {
-//           if(responseData){
-//                (responseData.json().then((data)=> {
-//                 console.log(data)
-//                })
-//             )}
-//         })
-//     }
-// }
-
-
-</script>
