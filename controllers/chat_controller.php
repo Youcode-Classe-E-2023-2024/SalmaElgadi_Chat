2f9fcc08fafb28cc70973916eb981c919c8b3af2
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-28 22:18:45 +0100

    Update chat_controller.php

--- controllers/chat_controller.php
+++ controllers/chat_controller.php
@@ -4,3 +4,15 @@ $Room = new Room();
 $id = $_GET['id'];
 $room = $Room->getRoomById($id);
 
+if(isset($_POST['quitterRoom']))
+{
+    $roomId = $_POST['roomId'];
+    $myId = $_POST['myId'];
+
+    if($Room->quitterRoom($roomId, $myId))
+    {
+        header("location:index.php?page=page1");
+    }
+
+}
+
