c7a44ee9a7c46e112bbb08a5ddd33914cfe47079
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-28 11:16:30 +0100

    Update chat_view.php

--- views/chat_view.php
+++ views/chat_view.php
@@ -5,14 +5,14 @@ if (!isset($_SESSION['id_user'])) {
 
 ?>
 
-<div class="flex flex-col items-center justify-center w-full h-1/2 text-gray-800 p-5">
+<div class="flex flex-col items-center justify-center w-full h-screen text-gray-800 p-5">
+    
     <!-- Component Start -->
     <div class="flex flex-col flex-grow w-full max-w-xl shadow-xl  rounded-lg overflow-hidden">
-    <div class="flex flex-col flex-grow h-0  overflow-auto">
+    <div class="flex flex-col flex-grow h-0 p-2 overflow-auto">
     <div class="fixed " style="width: 35rem;">
         <div class="py-2 rounded-md px-3 flex flex-row justify-between bg-purple-200 items-center">
 
-            <form id="sendMessageForm">
                 <!-- Room name -->
                 <div class="flex items-center">
                     <div>
@@ -32,15 +32,30 @@ if (!isset($_SESSION['id_user'])) {
                     </div>
                 </div>
                 <!--  -->
-            </form>
         </div>
-        </div>
+    </div>
 
-        <div id="chatContainer">
-        </div>
+    <div id="chatContainer">
+
+    </div>
+        <?php
+        foreach ($messages as $message) {
+        ?>
+
+            <div class="flex w-full mt-2 space-x-3 max-w-xs">
+				<div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+				<div>
+					<div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+						<p class="text-sm"><?php echo $message['text']; ?></p>
+					</div>
+				</div>
+			</div>
+        <?php
+        }
+        ?>
 
         <!-- inputs -->
-        <div class="bg-purple-100 pt-20 flex gap-2 w-full p-4">
+        <div class="bg-purple-100  flex content-end	 gap-2 w-full  p-4">
             <input id="messageInput" class="flex items-center h-10 w-3/4 rounded py-1 px-3 text-sm" type="text" placeholder="Type your message…">
             <div class="-mr-1 ">
                 <button id="sendMessageButton" class="bg-blue-200 text-gray-700 font-medium py-2 px-3 border border-gray-400 rounded-lg tracking-wide mr-1 hover:bg-gray-100" onclick="sendMessage()">Envoyer</button>
@@ -86,25 +101,40 @@ if (!isset($_SESSION['id_user'])) {
     }
 }
 
-function displayData(messages) {
-    console.log('Displaying data:', messages);
+//     function displayData(messages) {
+//     console.log('Displaying data:', messages);
 
-    const rows = messages.map((message) => {
-            return (`
-            <div class="flex-shrink-0 h-10 w-10 rounded-full bg-red-300">
-            <div>
-                <div class="bg-red-300 p-3 rounded-r-lg rounded-bl-lg">
-                    <p class="text-sm">${message.text}</p>
-                    <span class="text-xs text-gray-500 leading-none">zz</span>
-                </div>
-            </div>
-            </div>
-        `);
+//     const rows = messages.map((message) => {
+//             return (`
+//             <div class="flex w-full mt-2 space-x-3 max-w-xs">
+// 				<div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+// 				<div>
+// 					<div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+//                     <p class="text-sm">${message.text}</p>
+//                     </div>
+// 				</div>
+// 			</div>
+//         `);
+//     })
+//     document.getElementById('chatContainer').innerHTML = rows.join('');
 
+//     function ff(){
+//         var urlParams = new URLSearchParams(window.location.search);
+//         var id = urlParams.get('id');
+//         var url1 = `index.php?page=message&room=${id}`;
         
-    });
-    document.getElementById('chatContainer').innerHTML = rows.join('');
-}
+//         fetch(url1, {
+//             method: 'POST',
+//         })
+//         .then((responseData) => {
+//           if(responseData){
+//                (responseData.json().then((data)=> {
+//                 console.log(data)
+//                })
+//             )}
+//         })
+//     }
+// }
 
 
 </script>
