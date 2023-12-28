cb58c53496b26aa7839ce5b75c0fb7035cfdef5b
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-28 11:24:27 +0100

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,65 @@
-// const dropdownContent = document.querySelectorAll('.username'); 
-
-// const searchInput = document.getElementById('searchInput');
-// searchInput.addEventListener('input', filterOptions);
-
-// function filterOptions() {
-//     const searchTerm = searchInput.value.toLowerCase();
-
-//     dropdownContent.forEach(option => {
-//         const optionText = option.textContent.toLowerCase();
-//         option.style.display = optionText.includes(searchTerm) ? 'block' : 'none';
-//     });
-// }
+  
+    function sendMessage() {
+        const messageText = document.getElementById('messageInput').value.trim();
+        const roomIdInput = document.getElementById('roomId');
+        const roomId = roomIdInput.value;
+    
+        if (messageText !== '') {
+            fetch('index.php?page=chat', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    roomId: roomId,
+                    message: messageText,
+                }),
+            })
+            .then(response => response.json())
+            .then(response => {
+                console.log('Server response:', response);
+                if (response.success) {
+                    displayData(response.messages);
+                }
+            })
+            .catch(error => console.error('Error', error));
+    
+            document.getElementById('messageInput').value = '';
+        }
+    }
+    
+    //     function displayData(messages) {
+    //     console.log('Displaying data:', messages);
+    
+    //     const rows = messages.map((message) => {
+    //             return (`
+    //             <div class="flex w-full mt-2 space-x-3 max-w-xs">
+    // 				<div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+    // 				<div>
+    // 					<div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+    //                     <p class="text-sm">${message.text}</p>
+    //                     </div>
+    // 				</div>
+    // 			</div>
+    //         `);
+    //     })
+    //     document.getElementById('chatContainer').innerHTML = rows.join('');
+    
+    //     function ff(){
+    //         var urlParams = new URLSearchParams(window.location.search);
+    //         var id = urlParams.get('id');
+    //         var url1 = `index.php?page=message&room=${id}`;
+            
+    //         fetch(url1, {
+    //             method: 'POST',
+    //         })
+    //         .then((responseData) => {
+    //           if(responseData){
+    //                (responseData.json().then((data)=> {
+    //                 console.log(data)
+    //                })
+    //             )}
+    //         })
+    //     }
+    // }
+    
