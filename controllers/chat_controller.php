d449010b43edce2fbeadf1f67b2c6c418fea1315
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-28 11:38:41 +0100

    Update chat_controller.php

--- controllers/chat_controller.php
+++ controllers/chat_controller.php
@@ -3,27 +3,30 @@ include_once 'models/Room.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') 
     {
-    // Récupérer les données JSON envoyées depuis le client
-    $inputJSON = file_get_contents('php://input');
-    $data = json_decode($inputJSON, true);
+        // Récupérer les données JSON envoyées depuis le client
+        $inputJSON = file_get_contents('php://input');
+        $data = json_decode($inputJSON, true);
 
-    $roomId = $data['roomId'];
-    $message = $data['message'];
-    $myId = $_SESSION['id_user'];
+        $roomId = $data['roomId'];
+        $message = $data['message'];
+        $myId = $_SESSION['id_user'];
 
-    // Ajouter le message à la base de données
-    $Room = new Room();
-    if ($Room->sendMessage($myId, $roomId, $message)) {
-        echo json_encode(["success" => true]);
-    } else {
-        echo json_encode(["success" => false]);
+        $Room = new Room();
+            if ($Room->sendMessage($myId, $roomId, $message)) 
+            {
+                echo json_encode(["success" => true]);
+            } else 
+            {
+                echo json_encode(["success" => false]);
+            }
+
+        die();
     }
-    die();
-    }
+
     else{ 
         $Room = new Room();
-    $id = $_GET['id'];
-    $messages = $Room->getMessages($id);
-    echo json_encode(["messages" => $messages]);
-    $rooms = $Room->getRoomById($id);
+        $id = $_GET['id'];
+        $messages = $Room->getMessages($id);
+        // echo json_encode(["messages" => $messages]);
+        $rooms = $Room->getRoomById($id);
     }
